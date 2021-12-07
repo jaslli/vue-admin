a06e31955057bc524a95e76f5bbb80c7508f8f28
jaslli <1141950370@qq.com>
2021-12-07 22:31:12 +0800

    完善了一些细节

--- src/api/category.js
+++ src/api/category.js
@@ -7,7 +7,7 @@ import request from '@/utils/request'
  */
 export function selectAll() {
     return request({
-        url: '/category/slelctAll',
+        url: '/category/selelctAll',
         method: 'get'
     })
 }
